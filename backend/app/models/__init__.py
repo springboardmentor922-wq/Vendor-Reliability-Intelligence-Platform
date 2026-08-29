cef37cba8743d922078aa9d856f597fbe9c3d722
Aaryan Singh <Aaryan1005.tdl@gmail.com>
2026-08-29 12:25:41 +0530

    ;rgb:bfbf/bfbf/bfbfrgb:0000/0000/0000
    This reverts commit 062ad6a89af6e9f0047f0e3890b8d477d726baf7.

--- backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.models.user import User
-from app.models.password_reset import PasswordReset
-from app.models.vendor import Vendor
-
-# Add remaining models here as you build them (in FK dependency order):
-# procurement_requests, purchase_orders, invoices, vendor_performance,
-# reliability_scores, contracts, compliance_checks, messages,
-# activity_logs, notifications, reports
